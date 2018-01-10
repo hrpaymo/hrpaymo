1b8b92a6081fbf5b4dc903f62460786d49803d5d
acenine <acenine@gmx.com>
2018-01-10 14:51:49 -0800

    Create database dir with index.js file

---
+++ database/index.js
@@ -0,0 +1,1 @@
+//write database queries here 
---
+++ server/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const sampledata = require('../sampledata.js');
+const db = require('../database/index.js');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/feed/global', (req, res) => {
+
+  // DB query should replace default promise
+  Promise.resolve(sampledata.sampleFeed)
+    .then((results) => {
+      res.status(200).json({items: results});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(404);
+    })
+
+});
+
+app.get('/feed/user/:userId', (req, res) => {
+  
+  // DB query should replace default promise
+  let userId = req.params && req.params.userId;
+
+  Promise.resolve(sampledata.sampleFeed)
+    .then((results) => {
+      res.status(200).json({items: results});
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(404);
+    })
+});
+
+app.get('/login', (req, res) => {
+
+});
+
+module.exports = app;
+
