0189abbc4346437ce26a25f804671b8b65c5d17a
acenine <acenine@gmx.com>
2018-01-13 12:29:43 -0800

    Make color palette consistent

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -228,7 +228,7 @@ class App extends React.Component {
 
     return (
 
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <BrowserRouter>
             <Switch>
